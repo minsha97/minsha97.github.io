9cff3bd874db0b716997176bd796f0993679e446
minsha97 <60491730+minsha97@users.noreply.github.com>
2020-05-13 15:14:58 -0400

    Add files via upload

---
+++ js/main_page.js
@@ -0,0 +1,11 @@
+var background = document.getElementById("body")
+
+function changeBackground(){
+    background.style.backgroundColor = "#b4bfd1";
+}
+function changeBackground1(){
+    background.style.backgroundColor = "black";
+}
+function scrollPageUp() {
+    document.documentElement.scrollTop = 0;
+}
